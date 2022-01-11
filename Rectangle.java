340866cddd76ccee84243ee93ff7ce00d594d42b
Darji Jay <75724261+jayd972@users.noreply.github.com>
2022-01-11 15:52:18 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,7 @@
+package com.cognizant.shapes;
+
+public class Rectangle {
+	public void calculateArea(){
+		System.out.println("area of rectangle is calculated using the formula: length * breadth");
+	}
+}
